e9b367ede3d19bd9b028de30f4d00ecaa0501df0
omahecha38 <78917789+omahecha38@users.noreply.github.com>
2022-02-04 10:41:45 -0500

    Update set_pwm.m
    Added my name

--- set_pwm.m
+++ set_pwm.m
@@ -8,7 +8,7 @@ function action = set_pwm(device, pwm_value)
 %  ~ action: the control action to change the PWM
 %
 % Created by:  Kyle Naddeo 2/4/2022
-% Modified by: YOUR NAME and DATE
+% Modified by: Oscar Benitez and DATE 02/04/2022
 
 %% Force PWM value to be valid
 % pwm_value = % Bound value to limits 0 to 4095
