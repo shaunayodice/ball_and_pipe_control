f4f3a3292137ecd854ca33e9791746c15ba7756d
shaunayodice <98931471+shaunayodice@users.noreply.github.com>
2022-04-13 15:34:17 -0400

    update index

--- index.m
+++ index.m
@@ -6,22 +6,27 @@ state_spaceSize = perm(p_bins, v_bins);
 
 s1_bins=[0,1];
 s2_bins=[0,1];
+s3_bins = [0,1];
 
 index_table = zeros(state_size, state_v);
 
-s1Counter = 1;
-S2Counter = 1;
+s1_counter = 1;
+s2_counter = 1;
+s3_counter = 1;
 
 for i=1:Nrows(index_table)
-    index_table(i,:)=[s1_bins(s1Counter),s2_bins(S2Counter)];
+    index_table(i,:)=[s1_bins(s1_counter),s2_bins(s2_counter), s3_bins(s3_counter)];
+
+    s1_counter = s1_counter + 1;
 
     if (s1_counter > length(s1_bins))
 
-        s1_counter =1;
-        s2_counter =1;
+        s1_counter = 1;
+        s2_counter =s2_counter + 1;
        
         if(s2_counter > length(s2_bins))
-              s2_counter=1; 
+              s2_counter = 1; 
+              s3_counter = s3_counter + 1;
         end
 
     end
