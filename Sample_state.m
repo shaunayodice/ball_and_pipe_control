7820fadc9ac75a7fdc6ae10406a1429e5972cfb6
yazhinikumar <78919669+yazhinikumar@users.noreply.github.com>
2022-04-15 11:00:51 -0400

    Update Sample_state.m
    Added comment to the code

--- Sample_state.m
+++ Sample_state.m
@@ -1,29 +1,30 @@
 %% Sample State Function
 %
 %
-% Created By: Yaz Kumaravadivelan
+% Created By: Yazhini Kumaravadivelan
 %
 %
-% Modified By: Yaz
+% Modified By: Yazhini
 %
 %
 % Modified On: 4/9
 
 
 
-function Sample = Sample_state(P_bins , V_bins , F_bins) 
+function Sample = Sample_state(P_bins , V_bins , F_bins) % Creates a function called Sample_state that uses variables V_bins, F_bins and P_bins
 clear all;
 close all;
 
-Len_V = size(V_bins);
+Len_V = size(V_bins); % Len_V variable measures the size based of V_bin function.
 
-Len_F = size(F_bins);
+Len_F = size(F_bins); % Len_F variable measures the size based of F_bin function.
 
-Len_P = size(P_bins);
-P_sample = P_bins(rand(Len_P));
-V_sample = V_bins(rand(Len_V));
-F_sample = F_bins(rand(Len_F));
+Len_P = size(P_bins); % Len_P variable measures the size based of P_bin function.
+
+P_sample = P_bins(rand(Len_P)); % Formula for measureing the lenght of P bin.
+V_sample = V_bins(rand(Len_V)); % Formula for measureing the lenght of V bin.
+F_sample = F_bins(rand(Len_F));% Formula for measureing the lenght of F bin.
 
 
 
-Sample = cat(P_sample, V_sample, F_sample);
+Sample = cat(P_sample, V_sample, F_sample); % Concatination operation to create matrix using the P_sample, V_sample and F_sample/
