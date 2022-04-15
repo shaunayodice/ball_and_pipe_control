b78a7b47c0e244af99e03d76e7c718194b744c99
buchin84 <buchin84@students.rowan.edu>
2022-04-15 10:28:18 -0400

    Update binned_state.m
    comments and header

--- binned_state.m
+++ binned_state.m
@@ -1,5 +1,8 @@
-% % % binnedstate = cont_state
+%% Bins for all variables needed to store and change values 
+%% Written by Matthew Buchinski
+%% Modified on 4/15/2022
 
+% [p,v,f] %% where p = position, v = velocity, and f=fan speed
 p = 1 % calculated value
 v = 1 % calculated value
 f = 1 % calculated value
@@ -9,13 +12,10 @@ p,v,f = read_pwm
 
 cont_state = [p;v;f] % matrix
 
-% state = [p,v,f] %% position, velocity, fan speed
 % assign matrices 
-
 max_p = 0.95;
 min_p = 0;
-
-%max_v = dont know
+max_v = 50; % need to update
 
 P_bins = min_p:max_p/20:max_p % change values
 V_bins = 0:.1:.91 % change values
@@ -34,7 +34,6 @@ f_cont = cont_state(3)
 [~,vmin]=min((abs(V_bins - f_cont)))
 [~,fmin]=min((abs(F_bins - v_cont)))
 
-%'
 % takes result from matrix
 binned_p = P_bins(pmin)
 binned_v = V_bins(vmin)
@@ -46,9 +45,4 @@ u={binned_p,binned_v,binned_f}
 % concatenates min of all variables 
 binned_state = cat(1,u{:})
 
-%% example of concat 
-% a=[1 2 3]
-% b=[4 5 6]
-% c=[7 8 9]
-% u={a,b,c}
-% ans = cat(1,u{:})
+
