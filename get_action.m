ad2fdd877844e092b0744e0b79b32dfcf524aaf7
shaunayodice <98931471+shaunayodice@users.noreply.github.com>
2022-04-15 10:26:39 -0400

    added comments to get_Action

--- get_action.m
+++ get_action.m
@@ -1,7 +1,15 @@
-%% get action function
-%% created by Shaun Yodice
+%% Get Action Function
+%
+%
+% Purpose: This function either picks the best action based off the current
+% or it selects a random action and returns the action selected 
+%
+%
+% Created By: Shauna Yodice
+%
+%
 %$ Modifed on 4/15
-%% either picks the best action or a random action
+
 
 
 
@@ -12,7 +20,11 @@ success_rate = 1; % the probability of how often we do what we are supposed to d
 eps = 0.5; % initial epsilon
 eps_decay = 0.95; % decay factor after iteration
 
-aIdx = index(p_bins,v_bins,f_bins);
+aIdx = index(p_bins,v_bins,f_bins); % actions index
+
+% Here is where it picks the action. Checks the best action against the Q-
+% table which is exploitation or it picks a random action which is
+% exploration
 
 if (rand() > eps || episodes == num_episodes) && rand() <= success_rate
 
@@ -21,5 +33,6 @@ else
     aIdx = randi(length(actions),1); % random action selected
 end
 
+% update the epsilon value after the action is selected
 eps = eps*eps_decay;
 
